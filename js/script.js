3503f363fcf3e8c612ae93d40b07fe4d4526d888
brunEdo <edobruno.m@gmail.com>
2021-07-07 15:25:33 +0200

    script for changing logos and faqs

--- js/script.js
+++ js/script.js
@@ -19,3 +19,33 @@ annualyInput.addEventListener('change', () => {
 })
 
 
+// Changing the logo in the .big-square with the selected one
+const changingLogo = getId("changing-logo");
+
+const squares = getClass("square");
+for (let i = 0; i < squares.length; i++) {
+    squares[i].addEventListener('click', () => {
+        changingLogo.src = `img/${squares[i].id}.svg`;
+        changingLogo.alt = `${squares[i].id} logo`;
+    })
+};
+
+
+const faqsText = getClass("faq-text");
+const faqs = getClass("faq-btn");
+for (let i = 0; i < faqs.length; i++) {
+    faqs[i].addEventListener("click", () => {
+        if (! faqsText[i].classList.contains("show")) {
+            faqsText[i].classList.add("show")
+        } else if (faqsText[i].classList.contains("show")) {
+            faqsText[i].classList.remove("show");
+        }
+    })
+};
+// faqs[0].addEventListener("click", () => {
+//     if (! faqsText[0].classList.contains("show")) {
+//         faqsText[0].classList.add("show")
+//     } else if (faqsText[0].classList.contains("show")) {
+//         faqsText[0].classList.remove("show");
+//     }
+// })
